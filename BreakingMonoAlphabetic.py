7ba180c01bf93a589166c3b49536a94f41df5cbb
algaluva <52542906+algaluva@users.noreply.github.com>
2020-12-13 11:58:31 +0100

    cleaned up printed message for input and removed a debug line

--- BreakingMonoAlphabetic.py
+++ BreakingMonoAlphabetic.py
@@ -1,14 +1,12 @@
 #letter frequency in English
 enLetterFreq = {'E': 12.70, 'T': 9.06, 'A': 8.17, 'O': 7.51, 'I': 6.97, 'N': 6.75, 'S': 6.33, 'H': 6.09, 'R': 5.99, 'D': 4.25, 'L': 4.03, 'C': 2.78, 'U': 2.76, 'M': 2.41, 'W': 2.36, 'F': 2.23, 'G': 2.02, 'Y': 1.97, 'P': 1.93, 'B': 1.29, 'V': 0.98, 'K': 0.77, 'J': 0.15, 'X': 0.15, 'Q': 0.10, 'Z': 0.07}
 engFreqList = sorted(enLetterFreq.items(), key=lambda x: x[1], reverse=True)
-print("hey hey hey i am engFreqList", engFreqList)
 #latin alphabets for referrence
 latinAlphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 enFreqAlphabet = 'ETAOINSHRDLCUMWFGYPBVKJXQZ'
 
 #recieve encoded message through console
-print("Enter Encrypted Text")
-ecnryptedMsg = input()
+ecnryptedMsg = input('Enter Encrypted Text')
 
 #count letters in ecnryptedMsg
 def getLetterCount(message):
